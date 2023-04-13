2847ae3e8b98a545b0ee7cf8c2c0a9de568e607d
Ashley-Lijin <ashleylijin7@gmail.com>
2023-04-13 15:56:06 +0530

    addes new animatons

--- index.js
+++ index.js
@@ -1,10 +1,35 @@
 setTimeout(() => {
-  const main = document.querySelector("main");
-  main.style.opacity = 1;
-  main.style.transform = "translatey(0px)";
-}, 800);
-setTimeout(() => {
   const header = document.querySelector("header");
   header.style.opacity = 1;
   header.style.transform = "translatey(0px)";
+}, 600);
+
+setTimeout(() => {
+  const yt = document.querySelector(".yt");
+  yt.style.opacity = 1;
+  yt.style.transform = "translatey(0px)";
 }, 400);
+
+setTimeout(() => {
+  const ig = document.querySelector(".ig");
+  ig.style.opacity = 1;
+  ig.style.transform = "translatey(0px)";
+}, 600);
+
+setTimeout(() => {
+  const git = document.querySelector(".git");
+  git.style.opacity = 1;
+  git.style.transform = "translatey(0px)";
+}, 800);
+
+setTimeout(() => {
+  const med = document.querySelector(".med");
+  med.style.opacity = 1;
+  med.style.transform = "translatey(0px)";
+}, 1000);
+
+setTimeout(() => {
+  const header = document.querySelector("header");
+  header.style.opacity = 1;
+  header.style.filter = "blur(0px)";
+}, 1000);
