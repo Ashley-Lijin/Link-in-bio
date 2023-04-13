b6175806176aab5639082e6f459f579c2565e265
Ashley-Lijin <ashleylijin7@gmail.com>
2023-04-13 15:08:51 +0530

    removed animaton.css

---
+++ index.js
@@ -0,0 +1,10 @@
+setTimeout(() => {
+  const main = document.querySelector("main");
+  main.style.opacity = 1;
+  main.style.transform = "translatey(0px)";
+}, 800);
+setTimeout(() => {
+  const header = document.querySelector("header");
+  header.style.opacity = 1;
+  header.style.transform = "translatey(0px)";
+}, 400);
