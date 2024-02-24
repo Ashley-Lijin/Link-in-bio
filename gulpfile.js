973cb65f6bd37645e5d268fa6e29f4b841dd3bc1
Ashley lijin <ashleylijin@Ashleys-Mac-mini.local>
2024-02-24 23:52:00 +0530

    gulp and file organied

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+const { src, dest, watch, series, } = require("gulp")
+const sass = require("gulp-sass")(require("sass"))
+
+function buildStyles() {
+  return src('./src/index.scss')
+    .pipe(sass())
+    .pipe(dest('src/css'))
+}
+
+function watchTask() {
+  watch(['./src/index.scss'], buildStyles)
+}
+
+exports.default = series(buildStyles, watchTask)
